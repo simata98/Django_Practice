81a71a23534193d1aa804e9ec0e7eb8467c07f56
simata98 <wlsrnr905@naver.com>
2022-01-11 16:02:36 +0900

    today's menu time updated

--- costaurant/menus/views.py
+++ costaurant/menus/views.py
@@ -1,8 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-  return render(request, 'menus/index.html')
-
+  today = str(datetime.today().date())
+  context = {"today":today}
+  return render(request, 'menus/index.html', context=context)
